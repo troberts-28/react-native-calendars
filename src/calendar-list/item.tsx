7622468562572e5ec62ae3e82f4f74223daaca2a
Tim Roberts <tim.roberts20@imperial.ac.uk>
2023-07-26 15:10:40 +0100

    💄 fix styling for calendar placeholder

--- src/calendar-list/item.tsx
+++ src/calendar-list/item.tsx
@@ -1,6 +1,6 @@
 import XDate from 'xdate';
 import React, {useRef, useMemo, useCallback} from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {Theme} from '../types';
 import {toMarkingFormat} from '../interface';
 import {extractCalendarProps} from '../componentUpdater';
@@ -17,7 +17,7 @@ export type CalendarListItemProps = CalendarProps & {
   visible?: boolean;
 };
 
-const CalendarListItem = React.memo((props: CalendarListItemProps) => {  
+const CalendarListItem = React.memo((props: CalendarListItemProps) => {
   const {
     item,
     theme,
@@ -33,57 +33,66 @@ const CalendarListItem = React.memo((props: CalendarListItemProps) => {
   } = props;
 
   const style = useRef(styleConstructor(theme));
-  
+
   const calendarProps = extractCalendarProps(props);
   const dateString = toMarkingFormat(item);
-  
+
   const calendarStyle = useMemo(() => {
     return [
       {
         width: calendarWidth,
         minHeight: calendarHeight
-      }, 
+      },
       style.current.calendar,
       propsStyle
     ];
   }, [calendarWidth, calendarHeight, propsStyle]);
-  
-  const textStyle = useMemo(() => {
-    return [calendarStyle, style.current.placeholderText];
-  }, [calendarStyle]);
-  
-  const _onPressArrowLeft = useCallback((method: () => void, month?: XDate) => {
-    const monthClone = month?.clone();
-    if (monthClone) {
-      if (onPressArrowLeft) {
-        onPressArrowLeft(method, monthClone);
-      } else if (scrollToMonth) {
-        const currentMonth = monthClone.getMonth();
-        monthClone.addMonths(-1);
-        // Make sure we actually get the previous month, not just 30 days before currentMonth.
-        while (monthClone.getMonth() === currentMonth) {
-          monthClone.setDate(monthClone.getDate() - 1);
-        }
-        scrollToMonth(monthClone);
-      }
-    }
-  }, [onPressArrowLeft, scrollToMonth]);
 
-  const _onPressArrowRight = useCallback((method: () => void, month?: XDate) => {
-    const monthClone = month?.clone();
-    if (monthClone) {
-      if (onPressArrowRight) {
-        onPressArrowRight(method, monthClone);
-      } else if (scrollToMonth) {
-        monthClone.addMonths(1);
-        scrollToMonth(monthClone);
+  const _onPressArrowLeft = useCallback(
+    (method: () => void, month?: XDate) => {
+      const monthClone = month?.clone();
+      if (monthClone) {
+        if (onPressArrowLeft) {
+          onPressArrowLeft(method, monthClone);
+        } else if (scrollToMonth) {
+          const currentMonth = monthClone.getMonth();
+          monthClone.addMonths(-1);
+          // Make sure we actually get the previous month, not just 30 days before currentMonth.
+          while (monthClone.getMonth() === currentMonth) {
+            monthClone.setDate(monthClone.getDate() - 1);
+          }
+          scrollToMonth(monthClone);
+        }
       }
-    }
-  }, [onPressArrowRight, scrollToMonth]);
+    },
+    [onPressArrowLeft, scrollToMonth]
+  );
+
+  const _onPressArrowRight = useCallback(
+    (method: () => void, month?: XDate) => {
+      const monthClone = month?.clone();
+      if (monthClone) {
+        if (onPressArrowRight) {
+          onPressArrowRight(method, monthClone);
+        } else if (scrollToMonth) {
+          monthClone.addMonths(1);
+          scrollToMonth(monthClone);
+        }
+      }
+    },
+    [onPressArrowRight, scrollToMonth]
+  );
 
   if (!visible) {
     return (
-      <Text style={textStyle}>{dateString}</Text>
+      <View
+        style={[
+          calendarStyle,
+          {justifyContent: 'center', alignItems: 'center', flex: 1, width: '100%', backgroundColor: '#EDEDED'}
+        ]}
+      >
+        <Text style={style.current.placeholderText}>{item.toString('MMM yyyy')}</Text>
+      </View>
     );
   }
 
