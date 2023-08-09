65cfb6746ed035aa69572818b62d2f72e92a7e13
Tim Roberts <tim.roberts20@imperial.ac.uk>
2023-08-09 10:03:07 +0100

    🧑‍💻 remove example folders

--- example/src/screens/expandableCalendarScreen.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface Props {
-    weekView?: boolean;
-}
-declare const ExpandableCalendarScreen: (props: Props) => React.JSX.Element;
-export default ExpandableCalendarScreen;
--- example/src/screens/newCalendarListScreen.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-declare const NewCalendarListScreen: () => React.JSX.Element;
-export default NewCalendarListScreen;
